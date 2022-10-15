b05dec4cb1f02f516b650259a535126494680be0
Minkyu222341 <qkralsrb2040@naver.com>
2022-10-15 18:47:30 +0900

    2022-10-15

--- src/main/java/com/sparta/algorithm/result/Solution.java
+++ src/main/java/com/sparta/algorithm/result/Solution.java
@@ -7,16 +7,18 @@ import java.util.HashSet;
 class Solution {
   public int solution(int[] elements) {
     int answer = 0;
-    int count = 1;
+    int count = 0 ;
     HashSet<Object> num = new HashSet<>();
+
     for (int i = 0; i < elements.length; i++) {
-      num.add(elements[i]);
-      count++;
-      for (int j = 0; j < count; j++) {
-        num.add(elements[i] + elements[j]);
+      while (true) {
+        num.add(elements[i]);
+        count++;
       }
     }
+
     num.stream().sorted().forEach(System.out::println);
     return answer;
   }
+
 }
