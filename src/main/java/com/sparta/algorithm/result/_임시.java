7decaca77021cce2d20e03ff1cbd4167d9080ad4
Minkyu222341 <qkralsrb2040@naver.com>
2022-10-16 21:50:28 +0900

    2022-10-16

---
+++ src/main/java/com/sparta/algorithm/result/_임시.java
@@ -0,0 +1,33 @@
+//package com.sparta.algorithm.result;
+//
+//import java.util.Collection;
+//import java.util.Collections;
+//import java.util.HashSet;
+//
+//class Solution {
+//  public int solution(int[] elements) {
+//    int answer = 0;
+//    int count = 1;
+//    int sum =0 ;
+//    HashSet<Object> num = new HashSet<>();
+//
+//    while (true) {
+//      for (int i = 0; i < elements.length; i++) {
+//        num.add(elements[i]);
+//        for (int j = 0; j < i + count; j++) {
+//
+//        }
+//      }
+//
+//    }
+//
+//  }
+//
+//}
+//
+//public class Main {
+//  public static void main(String[] args) {
+//    Solution solution = new Solution();
+//    solution.solution(new int[]{7,1,1,9,4});
+//  }
+//}
