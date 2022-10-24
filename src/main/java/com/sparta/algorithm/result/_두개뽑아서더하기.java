5d2c176420668514d00e0e4dcb8a4ceb34d59f2c
Minkyu222341 <qkralsrb2040@naver.com>
2022-10-24 23:37:35 +0900

    2022-10-24

---
+++ src/main/java/com/sparta/algorithm/result/_두개뽑아서더하기.java
@@ -0,0 +1,36 @@
+package com.sparta.algorithm.result;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class _두개뽑아서더하기 {
+  public int[] solution(int[] num) {
+    int[] answer;
+    int count = 0;
+    HashMap<Integer, Integer> map = new HashMap();
+
+
+    for (int i = 0; i < num.length - 1; i++) {
+      for (int j = i + 1; j < num.length; j++) {
+        int sum = (num[i] + num[j]);
+
+        map.put(sum, map.getOrDefault(sum, 0));
+      }
+    }
+    answer = new int[map.size()];
+
+    for (int i : map.keySet()) {
+      answer[count] = i;
+      count++;
+    }
+
+    Arrays.sort(answer);
+
+    return answer;
+  }
+
+  public static void main(String[] args) {
+    final _두개뽑아서더하기 solution = new _두개뽑아서더하기();
+    solution.solution(new int[]{2, 1, 3, 4, 1});
+  }
+}
