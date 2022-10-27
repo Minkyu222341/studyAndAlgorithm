170a82aec42044dd0decea2e7608f1ad500d0e0b
Minkyu222341 <qkralsrb2040@naver.com>
2022-10-27 23:53:02 +0900

    2022-10-27

---
+++ src/main/java/com/sparta/algorithm/result/_수박수박.java
@@ -0,0 +1,20 @@
+package com.sparta.algorithm.result;
+
+public class _수박수박 {
+  public String solution(int n) {
+    String answer = "";
+    for(int i=0;i<n;i++){
+      if(i%2==0){
+        answer += "수";
+      }else{
+        answer +="박";
+      }
+    }
+
+    return answer;
+  }
+  public static void main(String[] args) {
+    final _수박수박 solution = new _수박수박();
+    solution.solution(5);
+  }
+}
