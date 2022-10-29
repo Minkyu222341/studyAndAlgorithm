b010b3837e83a85584a78adcc85a955b3e2bbf10
Minkyu222341 <qkralsrb2040@naver.com>
2022-10-30 07:11:02 +0900

    2022-10-30

---
+++ src/main/java/com/sparta/algorithm/result/_다음큰숫자.java
@@ -0,0 +1,24 @@
+package com.sparta.algorithm.result;
+
+public class _다음큰숫자 {
+
+  public int solution(int n) {
+    int answer = 0;
+    int requestNumberDecimalToBinaryCount = Integer.bitCount(n);
+    int flagNumber = n + 1;
+
+    for (int i = flagNumber; i <= 1000000; i++) {
+      int decimalToBinaryCount = Integer.bitCount(i);
+      if (requestNumberDecimalToBinaryCount == decimalToBinaryCount) {
+        answer = i;
+        break;
+      }
+    }
+    return answer;
+  }
+
+  public static void main(String[] args) {
+    final _다음큰숫자 solution = new _다음큰숫자();
+    solution.solution(78);
+  }
+}
