3f874a5f98a17705aecc2bb4f65f36fb6e6ec212
Minkyu222341 <qkralsrb2040@naver.com>
2023-01-25 08:47:51 +0900

    2023-01-25

---
+++ src/main/java/com/sparta/algorithm/result/programmers/lv2/_01_25_가장큰수.java
@@ -0,0 +1,40 @@
+package com.sparta.algorithm.result.programmers.lv2;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class _01_25_가장큰수 {
+    public String solution(int[] numbers) {
+        StringBuilder sb = new StringBuilder();
+
+        String[] numberToString = new String[numbers.length];
+
+        for (int i = 0; i < numbers.length; i++) {
+            numberToString[i] = String.valueOf(numbers[i]);
+        }
+
+        Arrays.sort(numberToString, new Comparator<String>() {
+            @Override
+            public int compare(String a, String b) {
+                return (b + a).compareTo(a + b);
+            }
+        });
+
+        if (numberToString[0].equals("0")) {
+            return 0+"";
+        }
+
+        for (String s : numberToString) {
+            sb.append(s);
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        _01_25_가장큰수 solution = new _01_25_가장큰수();
+//        solution.solution(new int[]{6, 10, 2});
+//        solution.solution(new int[]{3, 30, 34, 5, 9});
+        solution.solution(new int[]{0,0,0,0});
+    }
+}
