55c7e4fb272e7cb7ac97e24a42b37ce68ebeb888
Minkyu222341 <qkralsrb2040@naver.com>
2023-01-28 16:02:03 +0900

    리팩토링

--- src/main/java/com/sparta/algorithm/result/programmers/_01_28_위장.java
+++ src/main/java/com/sparta/algorithm/result/programmers/_01_28_위장.java
@@ -4,8 +4,6 @@ import java.util.HashMap;
 
 public class _01_28_위장 {
     public int solution(String[][] clothes) {
-        int num = 1;
-        int answer = 0;
         HashMap<String, Integer> map = new HashMap<>();
 
         insertData(clothes, map);
@@ -14,10 +12,7 @@ public class _01_28_위장 {
             return clothes.length;
         }
 
-        num = counting(num, map);
-
-
-        return num-1;
+        return counting(map) - 1;
     }
 
     private void insertData(String[][] clothes, HashMap<String, Integer> map) {
@@ -26,12 +21,13 @@ public class _01_28_위장 {
         }
     }
 
-    private int counting(int num, HashMap<String, Integer> map) {
+    private int counting(HashMap<String, Integer> map) {
+        int num = 1;
         for (Integer value : map.values()) {
             if (num == 0) {
                 num = value;
             }
-            num *= (value+1);
+            num *= (value + 1);
         }
         return num;
     }
@@ -40,6 +36,6 @@ public class _01_28_위장 {
     public static void main(String[] args) {
         _01_28_위장 solution = new _01_28_위장();
 //        solution.solution(new String[][]{{"yellow_hat", "headgear"},{ "blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}});
-        solution.solution(new String[][]{{"crow_mask", "face"},{ "blue_sunglasses", "face"}, {"smoky_makeup", "face"}});
+        solution.solution(new String[][]{{"crow_mask", "face"}, {"blue_sunglasses", "face"}, {"smoky_makeup", "face"}});
     }
 }
