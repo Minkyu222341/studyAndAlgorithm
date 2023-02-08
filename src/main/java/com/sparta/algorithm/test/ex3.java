95c18d5ee897c8d8a8c5af5b90b9a4a5a4d3b85a
Minkyu222341 <108817236+Minkyu222341@users.noreply.github.com>
2023-02-09 03:36:33 +0900

    Update ex3.java

--- src/main/java/com/sparta/algorithm/test/ex3.java
+++ src/main/java/com/sparta/algorithm/test/ex3.java
@@ -5,20 +5,6 @@ import java.util.*;
 public class ex3 {
     public int solution(int N, int[][] relation, String[] dirname) {
         int answer = 0;
-        Arrays.sort(relation, (o1, o2) -> o1[0] - o2[0]);
-        Map<Integer, String> map = new HashMap<>();
-        System.out.println(relation.length);
-        StringBuilder sb = new StringBuilder();
-        map.put(1, sb.append(dirname[0]).toString());
-        sb.setLength(0);
-        for (int i = 0; i < relation.length; i++) {
-            sb.append(map.get(relation[i][0])).append("/").append(dirname[relation[i][1] - 1]);
-            map.put(relation[i][1], sb.toString());
-            sb.setLength(0);
-        }
-        List<Integer> mapkey = new ArrayList<>(map.keySet());
-        mapkey.sort((o1, o2) -> map.get(o2).length() - map.get(o1).length());
-        answer = map.get(mapkey.get(0)).length();
         return answer;
     }
 
